e97704a8be4e07e1479e53f19368b0b665e73080
tlylt <liu_yongliang@hotmail.com>
2021-06-10 20:59:02 +0800

    Add images

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import htpImgUrl3 from '../assets/6.png'
 import htpArrow from "../assets/arrow.svg"
 import rEdu from "../assets/Educators_Guide.png"
 import rPlay from "../assets/How-To-Play Guide.png"
+import topChar from "../assets/2.png"
 
 document.getElementById('htp-1')?.setAttribute('src', htpImgUrl1)
 document.getElementById('htp-2')?.setAttribute('src', htpImgUrl2)
@@ -14,4 +15,6 @@ document.getElementById('htp-arrow-2')?.setAttribute('src', htpArrow)
 
 document.getElementById('r-play')?.setAttribute('src', rPlay)
 document.getElementById('r-edu')?.setAttribute('src', rEdu)
+
+document.getElementById('topChar')?.setAttribute('src', topChar)
 
