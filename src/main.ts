89c614aeeb85acac648ecbf1172a8d1398d0a9fd
tlylt <liu_yongliang@hotmail.com>
2021-06-10 09:45:23 +0800

    Add Resources section

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import htpImgUrl1 from '../assets/4.png'
 import htpImgUrl2 from '../assets/5.png'
 import htpImgUrl3 from '../assets/6.png'
 import htpArrow from "../assets/arrow.svg"
+import rEdu from "../assets/Educators_Guide.png"
+import rEdu1 from "../assets/Educators_Guide1.png"
 
 document.getElementById('htp-1')?.setAttribute('src', htpImgUrl1)
 document.getElementById('htp-2')?.setAttribute('src', htpImgUrl2)
 document.getElementById('htp-3')?.setAttribute('src', htpImgUrl3)
 document.getElementById('htp-arrow-1')?.setAttribute('src', htpArrow)
 document.getElementById('htp-arrow-2')?.setAttribute('src', htpArrow)
+
+document.getElementById('r-play')?.setAttribute('src', rEdu1)
+document.getElementById('r-edu')?.setAttribute('src', rEdu)
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     extend: {
       colors: {
         'HTP-bg': '#CADFDF',
-        'HTP-text': '#6C857C'
+        'HTP-text': '#6C857C',
+        'r-button': '#69C6C2',
+        'r-bg': '#F6E2DA',
+        'r-play-inner-bg': '#FEF3E6',
+        'r-play-border': '#F1CEA5',
+        'r-edu-inner-bg': '#E9EBEA'
+      },
+      boxShadow: {
+        button: '3px 3px #888888'
+      },
+      borderWidth: {
+        '10': '12px'
+      },
+      borderRadius: {
+        'xl': '2rem'
       }
     },
   },
